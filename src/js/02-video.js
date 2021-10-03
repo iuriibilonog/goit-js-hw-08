767c11dc130c21efc9d35be252dc58a7fffe3b7b
Iurii Bilonog <iuriibilonog@gmail.com>
2021-10-03 21:39:43 +0300

    done hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+'use strict';
+import vimeo from '@vimeo/player';
+import throttle from "lodash.throttle";
+
+const STORAGE_KEY = "videoplayer-current-time";
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+
+var onPlay = function (data) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data))
+   
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = localStorage.getItem(STORAGE_KEY) === null
+      ? 0 : localStorage.getItem(STORAGE_KEY);
+
+const parseCurrentTime = JSON.parse(currentTime);
+
+
+
+player.setCurrentTime(parseCurrentTime.seconds);
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+'use strict';
+
+import throttle from "lodash.throttle";
+
+
+const inputNode = document.querySelector('.feedback-form')
+const inputEmail = document.querySelector('.feedback-form input')
+const inputTextarea = document.querySelector('.feedback-form textarea')
+const inputBtn = document.querySelector('.feedback-form button')
+
+let obj = {};
+const STORAGE_KEY = "feedback-form-state";
+
+inputNode.addEventListener('input', throttle(inputText, 500));
+
+inputBtn.addEventListener('click', e => {
+  e.preventDefault();
+  console.log(obj);
+  localStorage.removeItem(STORAGE_KEY);
+  inputNode.reset();
+  
+} )
+  
+
+
+function inputText({ email, password }) {
+  email = inputEmail.value;
+  password = inputTextarea.value;
+
+  obj.email = email;
+  obj.password = password;
+  
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(obj))
+}
+
+
+
+
+
+if (localStorage.getItem(STORAGE_KEY)) {
+  const parseObj  = JSON.parse(localStorage.getItem(STORAGE_KEY))
+  inputEmail.value = parseObj.email 
+  inputTextarea.value = parseObj.password 
+}
 
