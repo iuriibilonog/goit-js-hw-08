8b270cb60e3904f706377cf4a7f2b19d9b035223
Iurii Bilonog <iuriibilonog@gmail.com>
2021-10-06 19:05:56 +0300

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,15 +7,7 @@ const STORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-
-var onPlay = function (data) {
+const onPlay = function (data) {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds));
 };
 
